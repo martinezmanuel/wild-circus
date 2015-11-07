79464d49904799973dddd406a61b072feba99cae
martinezmanuel <martinez-manuel@neuf.fr>
2015-11-07 22:04:47 +0100

    premier jet

---
+++ index.php
@@ -0,0 +1,34 @@
+﻿<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta content="text/html; charset=utf-8" http-equiv="content-type">
+    <title>Homepage </title>
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="css-site.css"type="text/css">
+	<meta name="decription" content="....">
+	<meta charset="utf-8">
+	
+	
+  </head>
+  <body>
+  <div id="site">
+  <header id="header">
+  <nav>
+	<ul id="main-nav" class="clearfix">
+	<li><a href="#">Home Page</a></li>
+	<li><a href="#">Perf1</a></li>
+	<li><a href="#">Perf2</a></li>
+	<li><a href="#">Perf3</a></li>
+	<li><a href="#">Perf4</a></li>
+	</ul>
+	</nav>
+	
+	
+  </header>
+   <div id="content">
+   </div>
+   <footer>
+   </footer>
+   </div>
+  </body>
+</html>
